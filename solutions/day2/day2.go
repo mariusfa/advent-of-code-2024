cf4f61c3e4bb28d139d96fad9d75d2687c538adc
Marius <mariusfagerland@yahoo.no>
2024-12-05 19:46:59 +0000

    Utkast til day 2 part 1

--- solutions/day2/day2.go
+++ solutions/day2/day2.go
@@ -1,5 +1,63 @@
 package day2
 
+// testdata
+// 7 6 4 2 1
+// 1 2 7 8 9
+// 9 7 6 2 1
+// 8 6 4 4 1
+// 1 3 2 4 5
+// 1 3 6 7 9
+
 func Solve() {
 	// TODO: Implement
+}
+
+func getSafeReports(reports [][]int) int {
+	safeCount := 0
+	for _, report := range reports {
+		pastValue := 0
+		isReportIncreasing := false
+
+		if report[0] == report[1] {
+			println("unsafe")
+			continue
+		}
+
+		if report[0] < report[1] {
+			println("increasing")
+			isReportIncreasing = true
+		} else {
+			println("decresing")
+			isReportIncreasing = false
+		}
+
+		isSafe := true
+		for index, value := range report {
+			println("value", value)
+			println("pastValue", pastValue)
+			if index == 0 {
+				pastValue = value
+				continue
+			}
+			if isReportIncreasing {
+				if value-pastValue < 1 && value-pastValue > 3 {
+					isSafe = false
+					break
+				}
+			} else {
+				println("value-pastValue", pastValue-value)
+				if pastValue-value < 1 && pastValue-value > 3 {
+					isSafe = false
+					break
+				}
+			}
+			pastValue = value
+		}
+		if isSafe {
+			println("safe")
+			safeCount++
+		}
+	}
+
+	return safeCount
 }
--- solutions/day2/day2_test.go
+++ solutions/day2/day2_test.go
@@ -2,6 +2,28 @@ package day2
 
 import "testing"
 
+// testdata
+// 7 6 4 2 1
+// 1 2 7 8 9
+// 9 7 6 2 1
+// 1 3 2 4 5
+// 8 6 4 4 1
+// 1 3 6 7 9
+
 func TestSafeReports(t *testing.T) {
-	// TODO: Implement
+	reports := [][]int{
+		{7, 6, 4, 2, 1},
+		{1, 2, 7, 8, 9},
+		{9, 7, 6, 2, 1},
+		{1, 3, 2, 4, 5},
+		{8, 6, 4, 4, 1},
+		{1, 3, 6, 7, 9},
+	}
+
+	safeReports := getSafeReports(reports)
+
+	expectedSafeReports := 2
+	if safeReports != expectedSafeReports {
+		t.Errorf("Expected safe reports to be %d, got %d", expectedSafeReports, safeReports)
+	}
 }
